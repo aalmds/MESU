a46a5a359157885ea6a28512b4733828f4ef837c
Andresa <aas10@cin.ufpe.br>
2022-07-22 13:48:57 -0300

    (feat): creating data from sensors

---
+++ module1/src/DataSender.h
@@ -0,0 +1,12 @@
+#ifndef MODULE1_DATA_SENDER 
+#define MODULE1_DATA_SENDER
+    #include <Wire.h>
+    #include <DS3231.h>   
+
+    class Sender{
+        public:
+        void sendData(){
+
+        }
+    };
+#endif
