7ac0a0784992b47df70be534b9f57d8e7b15ccee
Vinicius Novaes <vcn2@cin.ufpe.br>
2022-08-10 11:22:29 -0300

    - Sending data

--- module1/src/DataReader.h
+++ module1/src/DataReader.h
@@ -1,98 +1,111 @@
-#ifndef MODULE1_DATA_READER 
+#ifndef MODULE1_DATA_READER
 #define MODULE1_DATA_READER
 
-  #include <Wire.h>
-  #include <RtcDS3231.h> 
-  #include "Utils.h"
-  
-  RtcDS3231<TwoWire> rtc(Wire);
-  
-  using namespace Utils;
+#include <Wire.h>
+#include <RtcDS3231.h>
+#include "Utils.h"
 
-  class Reader{
-    String data;
+RtcDS3231<TwoWire> rtc(Wire);
 
-    String checkError(String data){
-      delay(500);
-      if(data != "-1"){
-        return " ";
-      }
+using namespace Utils;
 
-      for(int i = 0; i < NUM_SENSOR; ++i){
-        for(int j = i; j < NUM_SENSOR; ++j){
-          if(sensors[i] && !sensors[j]){
-            data += "_Sensor" + String(i);
-            break;
-          }
-        }
-      }
-      delay(500);
+class Reader
+{
+  String data;
 
-      return data;
+  String checkError(String data)
+  {
+    delay(500);
+    if (data != "-1")
+    {
+      return " ";
     }
 
-    String checkLevel(){
-      int level = -1;
-      for (int i = 0; i < NUM_LEVEL; ++i){
-        int count = 0;
-        for (int j = 0; j < NUM_SENSOR; ++j){
-          if (sensors[j] == levels[i][j]){
-            count++;
-          }
-        }
-        if (count == NUM_SENSOR){
-          level = i;
+    for (int i = 0; i < NUM_SENSOR; ++i)
+    {
+      for (int j = i; j < NUM_SENSOR; ++j)
+      {
+        if (sensors[i] && !sensors[j])
+        {
+          data += "_Sensor" + String(i);
           break;
         }
       }
-      return String(level);
     }
+    delay(500);
 
-    void readSensors(){
-      /*for (int i = 0; i < NUM_SENSOR; ++i){
-        sensors[i] = digitalRead(SENSOR1 + i);
+    return data;
+  }
+
+  String checkLevel()
+  {
+    int level = -1;
+    for (int i = 0; i < NUM_LEVEL; ++i)
+    {
+      int count = 0;
+      for (int j = 0; j < NUM_SENSOR; ++j)
+      {
+        if (sensors[j] == levels[i][j])
+        {
+          count++;
+        }
       }
-      */
+      if (count == NUM_SENSOR)
+      {
+        level = i;
+        break;
+      }
     }
+    return String(level);
+  }
 
-    /*
-    RtcDateTime readDateAndTime(){
-      rtc.Begin();
-      RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
+  void readSensors()
+  {
+    /*for (int i = 0; i < NUM_SENSOR; ++i){
+      sensors[i] = digitalRead(SENSOR1 + i);
+    }
+    */
+  }
 
-      if (!rtc.IsDateTimeValid()){
+  /*
+  RtcDateTime readDateAndTime(){
+    rtc.Begin();
+    RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
+
+    if (!rtc.IsDateTimeValid()){
+        rtc.SetDateTime(compiled);
+    } else{
+
+        RtcDateTime now = rtc.GetDateTime();
+        if (now < compiled){
           rtc.SetDateTime(compiled);
-      } else{
-  
-          RtcDateTime now = rtc.GetDateTime();
-          if (now < compiled){
-            rtc.SetDateTime(compiled);
-          }
-      }
-
-        rtc.Enable32kHzPin(false);
-        rtc.SetSquareWavePin(DS3231SquareWavePin_ModeNone);
-
-        return rtc.GetDateTime();
+        }
     }
 
-    String readArduinoId(){
-      // Arduino ID
-    }*/
+      rtc.Enable32kHzPin(false);
+      rtc.SetSquareWavePin(DS3231SquareWavePin_ModeNone);
 
-    public:
-    String readData(){
-      readSensors();
-      delay(500);
-      data += checkLevel();
-      delay(500);
-      data += checkError(data);
-      delay(500);
-      Serial.println(data);
-      delay(500);
-      /*data += readDateAndTime();
-      data += readArduinoId();*/
-      return data;
-    }
-  };
+      return rtc.GetDateTime();
+  }
+
+  String readArduinoId(){
+    // Arduino ID
+  }*/
+
+public:
+  String readData()
+  {
+    readSensors();
+    delay(500);
+    data += checkLevel();
+    delay(500);
+    data += checkError(data);
+    delay(500);
+    Serial.println(data);
+    delay(500);
+    /*data += readDateAndTime();
+    data += readArduinoId();*/
+    return data;
+  }
+};
 #endif
--- module1/src/DataSender.h
+++ module1/src/DataSender.h
@@ -1,17 +1,45 @@
-#ifndef MODULE1_DATA_SENDER 
+#ifndef MODULE1_DATA_SENDER
 #define MODULE1_DATA_SENDER
 
-    class Sender{
-        String information;
+SoftwareSerial ss(10, 11);
+SMW_SX1262M0 lorawan(ss);
 
-        public:
-        Sender(String data){
-            information = data;
+class Sender
+{
+    String information;
+
+public:
+    static CommandResponse response;
+
+    static const uint32_t P2P_FREQUENCY = 915200; // [kHz]
+
+    static const unsigned long PAUSE_TIME = 60000; // [ms] (1 min)
+    static unsigned long timeout;
+
+    Sender(String data)
+    {
+        information = data;
+    }
+
+    void sendData()
+    {
+        delay(1000);
+        Serial.println("SENDING DATA");
+        if (timeout < millis())
+        {
+            // send a message
+            response = lorawan.P2P_start(P2P_FREQUENCY, false, this->information.c_str()); // send only once
+            if (response == CommandResponse::OK)
+            {
+                Serial.println(F("Message sent"));
+            }
+            else
+            {
+                Serial.println(F("Error on sending the message"));
+            }
+
+            timeout = millis() + PAUSE_TIME;
         }
-        
-        void sendData(){
-            delay(1000);
-            Serial.println("SENDING DATA");
-        }
-    };
+    }
+};
 #endif
